c6c0281a183a76dbeedb5802d2418b002413eea1
priyankaj3w <juyal.priyanka94@gmail.com>
2022-06-23 16:09:55 +0530

    Update GitWorkflowActionsApplication.java

--- src/main/java/com/example/GitWorkflowActions/GitWorkflowActionsApplication.java
+++ src/main/java/com/example/GitWorkflowActions/GitWorkflowActionsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitWorkflowActionsApplication {
 
 	public static void main(String[] args) {
-		//Adding comment
+		//Adding commenting
 		SpringApplication.run(GitWorkflowActionsApplication.class, args);
 	}
 
