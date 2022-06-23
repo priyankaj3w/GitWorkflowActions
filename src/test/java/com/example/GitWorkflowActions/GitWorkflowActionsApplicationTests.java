02d91d4e2922642c6a8ece9f4cf1148847cddbc8
priyanka <juyal.priyanka94@gmail.com>
2022-06-23 16:00:33 +0530

    Adding base repo

---
+++ src/test/java/com/example/GitWorkflowActions/GitWorkflowActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitWorkflowActions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWorkflowActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
